30af3a8e4c43221c1e96221421dde9b5f4c4254c
Denis Efremov <efremov@novuscom.net>
2023-10-27 04:31:19 -0700

    Added result view

---
+++ TipCalculator/Extensions/UIView+Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  UIView+Extensions.swift
+//  TipCalculator
+//
+//  Created by Denis Efremov on 2023-10-26.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addShadow(offset: CGSize, color: UIColor, radius: CGFloat, opacity: Float) {
+        layer.cornerRadius = radius
+        layer.masksToBounds = false
+        layer.shadowOffset = offset
+        layer.shadowColor = color.cgColor
+        layer.shadowRadius = radius
+        layer.shadowOpacity = opacity
+        let backgroundCGColor = backgroundColor?.cgColor
+        backgroundColor = nil
+        layer.backgroundColor = backgroundCGColor
+    }
+
+}
